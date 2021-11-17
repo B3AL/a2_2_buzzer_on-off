73cef2abc0ce107fdf55c1415a6303746eb10a85
B3AL <92798364+B3AL@users.noreply.github.com>
2021-11-17 08:36:17 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
 })
+input.onButtonPressed(Button.B, function () {
+    music.ringTone(262)
+})
